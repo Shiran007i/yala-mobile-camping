145b48ab60dc10a2f63233f606dd50f11dcc136c
Shiran007i <shirantha007i@gmail.com>
2025-08-27 11:42:37 +0530

    vite updated

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ import { fileURLToPath, URL } from 'node:url';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/",
   
   // ADDED: Ensure title stays consistent and prevent "Vite + React" from showing
   define: {
